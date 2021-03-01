9d5e34c17e72343dcaea30a6f2fdcbe146f427f6
ClaytonSiby <claytonsiby@gmail.com>
2021-03-01 08:39:47 +0200

    Update & style the Quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 const Quote = () => (
-  <div>
-    <h2>Quote Page</h2>
+  <div className='quote'>
+    <p>
+      <cite>
+        Mathematics is not about numbers, equations, computations or algorithms:
+        it is about understanding.
+      </cite>{' '} <br />
+      <br />
+      -William Paul Thurston.
+    </p>
   </div>
-);
+)
 
-export default Quote;
+export default Quote
